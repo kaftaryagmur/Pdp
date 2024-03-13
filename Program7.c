4325ce1c88e662134926b0eae2d120826efc6099
kaftaryagmur <kaftaryagmur@gmail.com>
2024-03-13 21:54:15 +0300

    hafta 4

---
+++ Program7.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+
+int main(){
+	
+	
+	/*
+	//DO-WHILE DONGU
+	double toplamAgirlik =0,agirlik;
+	do{
+		printf("Agirlik: ");
+		scanf("%d",&agirlik);
+		if(agirlik <=0)continue;
+		if(toplamAgirlik+agirlik >100)break;
+		toplamAgirlik = toplamAgirlik+agirlik;
+	}while(toplamAgirlik<100);
+	printf("Toplam Agirlik: %1f ",toplamAgirlik);
+	*/
+	
+	
+	/*
+	//FOR DONGU
+	int a []={10,25,30,34,26,75,15,6};
+	printf("Aranan sayi: ");
+	int aranan;
+	scanf("%d",&aranan);
+	int index;
+	for( index=0;index<8;index++){
+		if(a[index]==aranan){
+			printf("Sayi var");
+			break;
+		}
+	}*/
+	return 0;
+}
